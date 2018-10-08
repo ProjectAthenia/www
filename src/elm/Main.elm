481345edf4f576cc830b2ca2c159fc0a24e42aeb
Bryce Meyer <bryce13950@gmail.com>
2018-10-08 02:39:40 +0200

    created base app

---
+++ src/elm/Main.elm
@@ -0,0 +1,7 @@
+-- Main entry point of the Lingwave admin
+module Lingwave.Main exposing (..)
+
+type Model
+    = NotFound
+
+
---
+++ src/index.js
@@ -0,0 +1,26 @@
+// pull in desired CSS/SASS files
+require( './scss/main.scss' );
+
+// inject bundled Elm app into div#main
+var Elm = require( './elm/Main' );
+
+var data = {
+    auth_token : window.localStorage.getItem('auth_token'),
+    retrieved_at : window.localStorage.getItem('retrieved_at'),
+    api_url: process.env.API_URL,
+    socket_url: process.env.SOCKET_URL,
+};
+// full screen the app and inject the env flags
+var app = Elm.Main.fullscreen(data);
+
+app.ports.setLocalStorageValue.subscribe(setLocalStorageValue);
+
+function setLocalStorageValue(data) {
+    window.localStorage.setItem(data[0], data[1]);
+}
+
+app.ports.clearStorage.subscribe(clearStorage);
+
+function clearStorage() {
+    window.localStorage.clear()
+}
