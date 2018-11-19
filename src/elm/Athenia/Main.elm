1fb0901da90f28c959727912a232b7e186f4980b
Bryce Meyer <bryce13950@gmail.com>
2018-11-19 11:24:32 -0400

    imported base page

--- src/elm/Athenia/Main.elm
+++ src/elm/Athenia/Main.elm
@@ -1,10 +1,23 @@
 -- Main entry point of the App
 module Athenia.Main exposing (..)
 
+import Athenia.Api as Api exposing (Token)
+import Athenia.Page.Login as Login
+import Athenia.Page.NotFound as NotFound
+import Athenia.Route exposing (Route)
+import Athenia.Session as Session exposing (Session)
 import Browser exposing (Document)
 
 type Model
-    = NotFound
+    = Redirect Session
+    | NotFound Session
+    | Home Home.Model
+    | Settings Settings.Model
+    | Login Login.Model
+    | Register Register.Model
+    | Profile Username Profile.Model
+    | Article Article.Model
+    | Editor (Maybe Slug) Editor.Model
 
 
 -- MODEL
