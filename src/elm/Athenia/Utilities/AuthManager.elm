2c5bfc35b3e029253d3bbb758c696190873bf87a
Bryce Meyer <bryce13950@gmail.com>
2018-12-23 00:16:12 -0400

    added auth manager

---
+++ src/elm/Athenia/Utilities/AuthManager.elm
@@ -0,0 +1,7 @@
+module Athenia.Utilities.AuthManager exposing (..)
+
+import Time
+
+needsRefresh : Time.Posix -> Int -> Bool
+needsRefresh currentTime lastRefreshTime =
+    Time.posixToMillis currentTime - (55 * 60 * 1000) >= lastRefreshTime
---
+++ tests/UnitTests/Utilities/AuthManagerTest.elm
@@ -0,0 +1,19 @@
+module UnitTests.Utilities.AuthManagerTest exposing (..)
+
+import Athenia.Utilities.AuthManager as AuthManager
+import Expect
+import Test exposing (..)
+import Time
+
+
+testNeedsRefresh =
+    describe "Makes sure that the needs refresh function works properly"
+        [ test "should return false at 5 minutes" <|
+            \() ->
+                Expect.false ""
+                    <| AuthManager.needsRefresh (Time.millisToPosix (5 * 60 * 1000)) 0
+        , test "should return true at 55 minutes" <|
+            \() ->
+                Expect.true ""
+                    <| AuthManager.needsRefresh (Time.millisToPosix (55 * 60 * 1000)) 0
+        ]
