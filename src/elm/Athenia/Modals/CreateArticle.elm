6c1efb7b1cd9089a77f0dc164720a9cfdd4c0699
Bryce Meyer <bryce13950@gmail.com>
2019-01-19 21:16:26 -0400

    added create article modal

---
+++ src/elm/Athenia/Modals/CreateArticle.elm
@@ -0,0 +1,107 @@
+module Athenia.Modals.CreateArticle exposing (..)
+
+import Bootstrap.Form.Input as Input
+import Bootstrap.Button as Button
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Modal as Modal
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+-- MODEL
+
+
+type alias Model =
+    { title : String
+    , visibility: Modal.Visibility
+    }
+
+
+init : Model
+init =
+    { title = ""
+    , visibility = Modal.hidden
+    }
+
+
+-- Create the view for the article creation modal
+view : Model -> Html Msg
+view model =
+    Modal.config Cancel
+        |> Modal.large
+        |> Modal.h4 [] [ text "Create Article" ]
+        |> Modal.body []
+            [ Grid.containerFluid []
+                [ Grid.row []
+                    [ Grid.col
+                        []
+                        [ Input.text
+                            [ Input.large
+                            , Input.placeholder "Enter Article Title"
+                            , Input.onInput EnteredTitle
+                            , Input.value model.title
+                            , Input.attrs [required True]
+                            ]
+                        ]
+                    ]
+                , Grid.row []
+                    [ Grid.col
+                        [ Col.xs6 ]
+                        [ Button.button
+                            [ Button.info
+                            , Button.onClick Cancel
+                            ]
+                            [ text "Cancel" ]
+                        ]
+                    , Grid.col
+                        [ Col.xs6 ]
+                        [ Button.button
+                            [ Button.danger
+                            , Button.onClick Confirm
+                            ]
+                            [ text "Submit" ]
+                        ]
+                    ]
+                ]
+            ]
+        |> Modal.view model.visibility
+
+
+
+-- UPDATE
+
+
+type Msg
+    = EnteredTitle String
+    | Cancel
+    | Confirm
+
+
+update: Msg -> Model -> Model
+update msg model =
+    case msg of
+        EnteredTitle title ->
+            { model
+                | title = title
+            }
+
+        Cancel ->
+            hide model
+
+        Confirm ->
+            hide model
+
+
+hide: Model -> Model
+hide model =
+    {model
+        | visibility = Modal.hidden
+    }
+
+
+show: Model -> Model
+show model =
+    {model
+        | visibility = Modal.shown
+    }
