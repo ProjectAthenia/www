e61193949bc4d94c7cf06af614afa150103210b4
Bryce Meyer <bryce13950@gmail.com>
2019-02-15 18:14:03 -0400

    added parsing of iteration action type

--- src/elm/Athenia/Models/Wiki/Iteration.elm
+++ src/elm/Athenia/Models/Wiki/Iteration.elm
@@ -1,14 +1,42 @@
 module Athenia.Models.Wiki.Iteration exposing (..)
 
 import Athenia.Models.User.User as User
+import Athenia.Utilities.StringHelper as StringHelper
 import Json.Decode as JsonDecode exposing (..)
 import Json.Decode.Pipeline exposing (..)
+
+
+
+type ActionType
+    = Add AddAction
+    | Remove RemoveAction
+    | Replace ReplaceAction
+    | NoAction
 
 
 type alias Model =
     { id : Int
     , content : String
     , created_by : Maybe User.Model
+    }
+
+
+type alias AddAction =
+    { start_position: Int
+    , content: String
+    }
+
+
+type alias RemoveAction =
+    { start_position: Int
+    , length: Int
+    }
+
+
+type alias ReplaceAction =
+    { start_position: Int
+    , length: Int
+    , content: String
     }
 
 
@@ -24,3 +52,62 @@ modelDecoder =
 listDecoder : Decoder (List Model)
 listDecoder =
     JsonDecode.list modelDecoder
+
+
+getContentActionType: String -> String -> ActionType
+getContentActionType previousContent newContent =
+    let
+        maybeFirstDifferentPosition
+            = StringHelper.findFirstNoneMatchingStringPosition previousContent newContent
+        maybeLastDifferentPosition
+            = StringHelper.findLastNoneMatchingStringPosition previousContent newContent
+    in
+    case (maybeFirstDifferentPosition, maybeLastDifferentPosition) of
+        (Just firstDifferentPosition, Just lastDifferentPosition) ->
+            let
+                previousContentLastDifferentPosition
+                    = (String.length previousContent) - lastDifferentPosition
+                newContentLastDifferentPosition
+                    = (String.length newContent) - lastDifferentPosition
+            in
+            if String.length newContent >= String.length previousContent then
+                let
+                    -- If the first different character and the last different character are the same,
+                    -- then the following character will be detected as different, so we need to add a 1 character
+                    -- buffer to certain variables
+                    buffer =
+                        if String.slice firstDifferentPosition 1 newContent == String.slice newContentLastDifferentPosition 1 newContent then
+                            1
+                        else
+                            0
+                in
+                -- the position where things are different have the same position going forwards and backwards
+                if firstDifferentPosition == previousContentLastDifferentPosition + buffer then
+                    Add
+                        { start_position = firstDifferentPosition
+                        , content = String.slice firstDifferentPosition (newContentLastDifferentPosition + buffer) newContent
+                        }
+                else
+                    Replace
+                        { start_position = firstDifferentPosition
+                        , length = previousContentLastDifferentPosition - firstDifferentPosition
+                        , content = String.slice firstDifferentPosition newContentLastDifferentPosition newContent
+                        }
+
+            else
+                -- the position where things are different have the same position going forwards and backwards
+                if firstDifferentPosition == newContentLastDifferentPosition then
+                    Remove
+                        { start_position = firstDifferentPosition
+                        , length = previousContentLastDifferentPosition - firstDifferentPosition
+                        }
+                else
+                    Replace
+                        { start_position = firstDifferentPosition
+                        , length = previousContentLastDifferentPosition - firstDifferentPosition
+                        , content = String.slice firstDifferentPosition newContentLastDifferentPosition newContent
+                        }
+
+        _ ->
+            NoAction
+
--- tests/UnitTests/Models/Wiki/IterationTest.elm
+++ tests/UnitTests/Models/Wiki/IterationTest.elm
@@ -29,3 +29,28 @@ testModelDecoder =
                                 })
                     <| JsonDecode.decodeString Iteration.modelDecoder "{\"id\":342,\"content\":\"Some Content\",\"created_by\":{\"id\":53,\"name\":\"Barry Manilow\",\"email\":\"butts@butts.com\"}}"
         ]
+
+testGetContentActionType: Test
+testGetContentActionType =
+    describe "Tests to make sure that the right content action can be determined"
+        [ test "make sure that a matching string returns properly" <|
+            \() ->
+                Expect.equal Iteration.NoAction
+                    <| Iteration.getContentActionType "A piece of content." "A piece of content."
+        , test "make sure that an add action returns properly" <|
+            \() ->
+                Expect.equal (Iteration.Add {start_position = 2, content = "new "})
+                    <| Iteration.getContentActionType "A piece of content." "A new piece of content."
+        , test "make sure that a remove action returns properly" <|
+            \() ->
+                Expect.equal (Iteration.Remove {start_position = 1, length = 5})
+                    <| Iteration.getContentActionType "An old piece of content." "A piece of content."
+        , test "make sure that a replace action returns properly" <|
+            \() ->
+                Expect.equal (Iteration.Replace {start_position = 1, length = 7, content = " new" })
+                    <| Iteration.getContentActionType "An older piece of content." "A new piece of content."
+        , test "make sure that a replace action returns properly when the content is shorter" <|
+            \() ->
+                Expect.equal (Iteration.Replace {start_position = 2, length = 3, content = "recent" })
+                    <| Iteration.getContentActionType "A new piece of content." "A recent piece of content."
+        ]
