5e3a26e6233c6bfd3da88a169ad1bc6a0cd0f834
Bryce Meyer <bryce13950@gmail.com>
2019-03-29 00:41:56 -0600

    added membership plan model

---
+++ src/elm/Models/MembershipPlan/MembershipPlan.elm
@@ -0,0 +1,24 @@
+module Models.MembershipPlan.MembershipPlan exposing (..)
+
+import Json.Decode as JsonDecode exposing (..)
+import Json.Decode.Pipeline exposing (..)
+
+
+type alias Model =
+    { id: Int
+    , name: String
+    , duration: String
+    , current_cost: Float
+    , current_rate_id: Int
+    }
+
+
+-- Decodes a user model retrieved through the API
+modelDecoder : Decoder Model
+modelDecoder =
+    JsonDecode.succeed Model
+        |> required "id" int
+        |> required "name" string
+        |> required "duration" string
+        |> required "current_cost" float
+        |> required "current_rate_id" int
---
+++ tests/UnitTests/Models/MembershipPlan/MembershipPlanTest.elm
@@ -0,0 +1,18 @@
+module UnitTests.Models.MembershipPlan.MembershipPlanTest exposing (..)
+
+import Models.MembershipPlan.MembershipPlan as MembershipPlan
+import Expect
+import Json.Decode as JsonDecode
+import Test exposing (..)
+
+testModelDecoder : Test
+testModelDecoder =
+    test "Test decode" <|
+        \() ->
+            Expect.equal (Ok { id = 342
+                            , name = "Steve"
+                            , duration = "yearly"
+                            , current_cost = 12.12
+                            , current_rate_id = 345
+                            })
+                <| JsonDecode.decodeString MembershipPlan.modelDecoder "{\"id\":342,\"name\":\"Steve\",\"duration\":\"yearly\",\"current_cost\":12.12,\"current_rate_id\":345}"
