7ffebeade93e819d08ff24f9cb1e131574ec04f5
Bryce Meyer <bryce13950@gmail.com>
2020-07-18 20:05:04 +0200

    fixed test

--- tests/UnitTests/MainTest.elm
+++ tests/UnitTests/MainTest.elm
@@ -3,6 +3,7 @@ module UnitTests.MainTest exposing (..)
 import Expect
 import Main
 import Models.Role as Role
+import Page.Admin.Root as Admin
 import Route
 import Test exposing (..)
 import UnitTests.Models.User.UserTest as UserTest
@@ -21,6 +22,7 @@ testGetNavItems =
             \() ->
                 Expect.equal (Main.getNavItems (Just ()) Nothing)
                     [ ("Settings", Route.href  Route.Settings)
+                    , ("Admin", Route.href (Route.Admin Admin.Dashboard))
                     , ("Log Out", Route.href  Route.Logout)
                     ]
         , test "Should Return the logged in nav with a token, and with a user that cannot view articles" <|
@@ -30,6 +32,7 @@ testGetNavItems =
                 in
                 Expect.equal (Main.getNavItems (Just ()) (Just user))
                     [ ("Settings", Route.href  Route.Settings)
+                    , ("Admin", Route.href (Route.Admin Admin.Dashboard))
                     , ("Log Out", Route.href  Route.Logout)
                     ]
         , test "Should Return the logged in nav with a token, and with a user that can view articles" <|
@@ -40,6 +43,7 @@ testGetNavItems =
                 Expect.equal (Main.getNavItems (Just ()) (Just user))
                     [ ("Browse Articles", Route.href Route.Articles)
                     , ("Settings", Route.href  Route.Settings)
+                    , ("Admin", Route.href (Route.Admin Admin.Dashboard))
                     , ("Log Out", Route.href  Route.Logout)
                     ]
         ]
